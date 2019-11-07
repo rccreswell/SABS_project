fd92dedba2f7631816120526cd98d5e2f53665b4
me <me@example.com>
2019-11-07 16:25:11 +0000

    commited tests

--- sabs_pkpd/run_model.py
+++ sabs_pkpd/run_model.py
@@ -7,11 +7,9 @@ def set_myokit_simulation(model_filename):
     return myokit.Simulation(model, prot)
 
 
-def simulate_data(fitted_params, params_values, exp_param, s, read_out: str, data):
+def simulate_data(fitted_params, params_values, exp_param, s, read_out: str, data, pre_run = 0):
 
-    # Set parameters for simulation
-    for i in range(0, len(fitted_params)):
-        s.set_constant( fitted_params[i], params_values[i])
+    # This function is meant for comparison of simulation conditions to data, or to be called by the PINTS optimisation tool
 
     # Allocate memory for the output
     output = []
@@ -22,11 +20,80 @@ def simulate_data(fitted_params, params_values, exp_param, s, read_out: str, dat
         # reset timer
         s.set_time(0)
 
+        # Set parameters for simulation
+        for i in range(0, len(fitted_params)):
+            s.set_constant(fitted_params[i], params_values[i])
+
         # set the right experimental conditions
         s.set_constant(exp_param, list(set(data.exp_conds))[k])
+
+        # Eventually run a pre-run to reach steady-state
+        s.pre(pre_run)
 
         # Run the simulation with starting parameters
         a = s.run(data.times[k][-1]+0.01, log_times=data.times[k])
         # Convert output in concentration
         output.append( list(a[read_out]))
+    return output
+
+
+def quick_simulate(s, time_max, read_out: str,  varying_param_name = None, varying_param_values = [], fixed_params_names = [], fixed_params_values = [], pre_run = 0, time_samples = []):
+
+    '''This function is for quick simulation of user selected conditions
+    '''
+
+    if len(fixed_params_names) != len(fixed_params_values):
+        raise ValueError('The parameters clamped for the simulation must have the same length for names and values')
+
+    if len(time_samples) == 0:
+        time_samples = np.linspace(0,time_max, 100)
+
+    # Allocate memory for the output
+    output = []
+
+    # Run the model solving for all experiment conditions
+    # In case the user wants some parameter to vary between simulations
+    if len(varying_param_values) > 0:
+        for k in range(0, len(varying_param_values)):
+            s.reset()
+
+            # Set parameters for simulation
+            if len(fixed_params_names) > 0:
+                for i in range(0, len(fixed_params_names)):
+                    s.set_constant(fixed_params_names[i], fixed_params_values[i])
+
+            # set the right experimental conditions
+            s.set_constant(varying_param_name, varying_param_values[k])
+
+            # reset timer
+            s.set_time(0)
+
+            # Eventually run a pre-run to reach steady-state
+            s.pre(pre_run)
+
+            # Run the simulation with starting parameters
+            a = s.run(time_max, log_times=time_samples)
+            # Convert output in concentration
+            output.append( list(a[read_out]))
+
+    # In case there is no looping over a v varying parameter
+    else:
+        s.reset()
+
+        # Set parameters for simulation
+        if len(fixed_params_names) > 0:
+            for i in range(0, len(fixed_params_names)):
+                s.set_constant(fixed_params_names[i], fixed_params_values[i])
+
+        # reset timer
+        s.set_time(0)
+
+        # Eventually run a pre-run to reach steady-state
+        s.pre(pre_run)
+
+        # Run the simulation with starting parameters
+        a = s.run(time_max, log_times=time_samples)
+        # Convert output in concentration
+        output.append(list(a[read_out]))
+
     return output
--- tests/test_run_model.py
+++ tests/test_run_model.py
@@ -19,3 +19,20 @@ def test_simulate_data():
     diff = np.array(out) - np.array([[0.0, 0.01975, 0.09404, 0.17719, 0.42628, 0.58513, 0.79126, 0.99661],
                                      [0.0, 0.019504, 0.08836, 0.15683, 0.30589, 0.35623, 0.37456, 0.37037]])
     assert np.linalg.norm(diff) < 0.0001
+
+
+def test_quick_simulate():
+    s = sabs_pkpd.run_model.set_myokit_simulation('./tests/test resources/pints_problem_def_test.mmt')
+    time_max = 1
+    changed_params_names = ['constants.unknown_cst', 'constants.unknown_cst2']
+    changed_params_values = [0.1, 0.1]
+    time_samples = [0, 0.01, 0.05, 0.1, 0.3, 0.5, 1, 5]
+
+    test1 = sabs_pkpd.run_model.quick_simulate(s, time_max, 'comp1.y')
+    expected_value = np.array([0.0, 0.00691, 0.00643, 0.00585, 0.00559, 0.00550, 0.00547, 0.00546, 0.00546, 0.00545, 0.00545, 0.005454])
+    diff = np.array(test1[0])[0:11] - expected_value
+    assert np.linalg.norm(diff) < 0.0001
+
+    test2 = sabs_pkpd.run_model.quick_simulate(s, time_max, 'comp1.y', time_samples=time_samples, fixed_params_names=changed_params_names, fixed_params_values=changed_params_values)
+    diff = np.array(test2[0]) - np.array([0.0, 0.019504, 0.08836, 0.15683, 0.30589, 0.35623, 0.37456, 0.37037])
+    assert np.linalg.norm(diff) < 0.0001
