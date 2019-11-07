6398a739ca00b700b45f116f9c319a7b61837080
me <me@example.com>
2019-11-07 14:18:04 +0000

    commited files

--- sabs_pkpd/run_model.py
+++ sabs_pkpd/run_model.py
@@ -30,17 +30,3 @@ def simulate_data(fitted_params, params_values, read_out: str, data):
         # Convert output in concentration
         output.append( list(a[read_out]))
     return output
-
-s = set_myokit_simulation('C:/Users/yanral/Documents/Software Development/tests/test resources/pints_problem_def_test.mmt')
-
-data = load_data.load_data_file('C:/Users/yanral/Documents/Software Development/tests/test resources/load_data_test.csv')
-
-fitted_params = ['constants.unknown_cst', 'constants.unknown_cst2']
-params_values = [0.1,0.1]
-read_out = 'comp1.y'
-exp_param = 'constants.T'
-exp_cond = [20, 37]
-
-out = simulate_data(fitted_params, params_values,read_out, data)
-diff = np.array(out) - np.array([[0.0, 0.01975, 0.09404, 0.17719, 0.42628, 0.58513, 0.79126, 0.99661], [0.0, 0.019504, 0.08836, 0.15683, 0.30589, 0.35623, 0.37456, 0.37037]])
-print(np.linalg.norm(diff))
