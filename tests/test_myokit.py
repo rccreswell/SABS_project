8cd619ff6c39573eea2a879c7b4b61c1634b1797
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2019-11-07 11:55:50 +0000

    test myokit install

---
+++ tests/test_myokit.py
@@ -0,0 +1,10 @@
+import myokit
+
+def test_myokit_import():
+    """Test that Myokit can be loaded.
+
+    Uses the Myokit builtin example.
+    """
+    m, p, x = myokit.load('example')
+    s = myokit.Simulation(m, p)
+    assert True
