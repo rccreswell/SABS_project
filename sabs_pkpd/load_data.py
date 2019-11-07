c095d6da9e178397fe9bfd03b488e72600f532fe
me <me@example.com>
2019-11-07 09:41:07 +0000

    tests

--- sabs_pkpd/load_data.py
+++ sabs_pkpd/load_data.py
@@ -28,18 +28,26 @@ class Data_exp():
 def load_data_file(filename, headers: bool = True):
 
     # Data should be provided in 4 columns : time, data, experiment number, experiment condition,
-    data = np.loadtxt(filename, delimiter = ',', skiprows = int(headers))
+    data = np.loadtxt(filename, delimiter= ',', skiprows= int(headers))
 
     if type(data[0][0]) == str :
         raise ValueError('The CSV file is not in the standard format. Please refer to the documentation. (More than one line of headers)')
     if len(data[0]) > 4:
         raise ValueError('The CSV file is not in the standard format. Please refer to the documentation. (Too many columns)')
 
-    #Sorting the list in increasing times and experiment number
+    # Sorting the list in increasing times and experiment number
 
     data = sorted(data, key=itemgetter(2, 0))
     data = np.concatenate([i for i in data])
     data = data.reshape(len(data)//4, 4)
+    exp_nums_list = list(set(data[:,2]))
+    exp_conds_list = list(set(data[:, 3]))
+    times = []
+    values = []
 
-    return Data_exp(data[:, 0], data[:, 1], data[:, 2], data[:, 3])
+    for i in range(len(exp_nums_list)):
+        temp = data[ data[:, 2] == exp_nums_list[i] ]
+        times.append(temp[:,0])
+        values.append(temp[:,1])
 
+    return Data_exp(times,values, exp_nums_list, exp_conds_list)
