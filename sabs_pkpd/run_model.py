66d31b134579d9ecfe7a83e9307c4872bf63db05
me <me@example.com>
2019-11-07 11:03:03 +0000

    added the equations we agreed on to the .mmt file

--- sabs_pkpd/run_model.py
+++ sabs_pkpd/run_model.py
@@ -1,18 +1,18 @@
 import pints
 import myokit
 import numpy as np
-from . import load_data
+import load_data
 
 def set_myokit_simulation(model_filename):
     model, prot, script = myokit.load(model_filename)
     return myokit.Simulation(model, prot)
 
 
-def simulate(self, selected_params, params_values, data):
+def simulate(self, fitted_params, params_values, data):
     s.reset()
 
     # Set parameters for simulation
-    for i in range(len(selected_params)):
+    for i in range(len(fitteds_param)):
         eval('s.set_constant(' + selected_params[i] + ', ' + str(params_values[i]) + ')')
 
     # Run the model solving for all experiment conditions
@@ -21,7 +21,7 @@ def simulate(self, selected_params, params_values, data):
         s.set_time(0)
 
         # set the right experimental conditions
-        eval('s.set_constant(' + exp_cond[i] + ', ' + str(list(set(data.exp_conds))[i]) + ')')
+        eval('s.set_constant(' + exp_param + ', ' + str(list(set(data.exp_conds))[i]) + ')')
         s.set_constant('', dose[i])
 
         # Run the simulation with starting parameters
@@ -35,5 +35,8 @@ def simulate(self, selected_params, params_values, data):
 
 s = set_myokit_simulation('C:/Users/yanral/Documents/Software Development/tests/test resources/pints_problem_def_test.mmt')
 
-selected_params = ['constants.unknown_cst']
+fitted_params = ['constants.unknown_cst']
 data = load_data.load_data_file('C:/Users/yanral/Documents/Software Development/tests/test resources/load_data_test.csv')
+print(data.times)
+exp_param = 'constants.T'
+exp_cond = [20, 37]
