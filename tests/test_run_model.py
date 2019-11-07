1749e37795d8ed4a68234d9e05892ac29de855d7
me <me@example.com>
2019-11-07 14:52:25 +0000

    commited tests

--- tests/test_run_model.py
+++ tests/test_run_model.py
@@ -7,7 +7,7 @@ import numpy as np
 def test_simulate_data():
     s = sabs_pkpd.run_model.set_myokit_simulation('./tests/test resources/pints_problem_def_test.mmt')
 
-    data = sabs_pkpd.load_data.load_data_file('./test resources/load_data_test.csv')
+    data = sabs_pkpd.load_data.load_data_file('./tests/test resources/load_data_test.csv')
 
     fitted_params = ['constants.unknown_cst', 'constants.unknown_cst2']
     params_values = [0.1, 0.1]
