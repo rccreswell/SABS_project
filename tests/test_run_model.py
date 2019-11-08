b8ec3e87c7b8d97860946c38c11ae04fa874ae2e
me <me@example.com>
2019-11-08 16:21:43 +0000

    commited tests

--- tests/test_run_model.py
+++ tests/test_run_model.py
@@ -7,15 +7,16 @@ import numpy as np
 def test_simulate_data():
     s = sabs_pkpd.run_model.set_myokit_simulation('./tests/test resources/pints_problem_def_test.mmt')
 
-    data = sabs_pkpd.load_data.load_data_file('./tests/test resources/load_data_test.csv')
+    data_exp = sabs_pkpd.load_data.load_data_file('./tests/test resources/load_data_test.csv')
 
-    fitted_params = ['constants.unknown_cst', 'constants.unknown_cst2']
-    params_values = [0.1, 0.1]
-    read_out = 'comp1.y'
-    exp_param = 'constants.T'
-    exp_cond = [20, 37]
+    fitted_params_annot = ['constants.unknown_cst', 'constants.unknown_cst2']
+    fitted_params_values = [0.1, 0.1]
+    sim_output_param_annot = 'comp1.y'
+    exp_cond_param_annot = 'constants.T'
 
-    out = sabs_pkpd.run_model.simulate_data(fitted_params, params_values, exp_param, s, read_out, data)
+    data_exp.Add_fitting_instructions(fitted_params_annot, exp_cond_param_annot, sim_output_param_annot)
+
+    out = sabs_pkpd.run_model.simulate_data(fitted_params_values, s, data_exp)
     diff = np.array(out) - np.array([[0.0, 0.01975, 0.09404, 0.17719, 0.42628, 0.58513, 0.79126, 0.99661],
                                      [0.0, 0.019504, 0.08836, 0.15683, 0.30589, 0.35623, 0.37456, 0.37037]])
     assert np.linalg.norm(diff) < 0.0001
@@ -33,7 +34,7 @@ def test_quick_simulate():
     diff = np.array(test1[0])[0:12] - expected_value
     assert np.linalg.norm(diff) < 0.002
 
-    test2 = sabs_pkpd.run_model.quick_simulate(s, time_max, 'comp1.y', time_samples=time_samples, fixed_params_names=changed_params_names, fixed_params_values=changed_params_values)
+    test2 = sabs_pkpd.run_model.quick_simulate(s, time_max, 'comp1.y', time_samples=time_samples, fixed_params_annot=changed_params_names, fixed_params_values=changed_params_values)
 
     diff = np.array(test2[0]) - np.array([0.0, 0.019504, 0.08836, 0.15683, 0.30589, 0.35623, 0.37456, 0.37037])
     assert np.linalg.norm(diff) < 0.0001
