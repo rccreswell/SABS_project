46bcb5c7f2b52c5a2990df54f3a5c0910fe70302
me <me@example.com>
2019-11-08 09:40:18 +0000

    used clearer variable names

--- sabs_pkpd/pints_problem_def.py
+++ sabs_pkpd/pints_problem_def.py
@@ -3,6 +3,15 @@ import myokit
 import numpy as np
 import sabs_pkpd
 
+class FittingInstructions():
+
+    def __init__(self, fitted_params_annot, fitted_params_values, varying_param_annot, sim_output_param_annot):
+        self.fitted_params_annot = []
+        self.fitted_params_values = []
+        self.varying_param_annot = []
+        self.sim_output_param_annot = []
+
+
 class MyModel(pints.ForwardModel):
     def n_parameters(self):
         # Define the amount of fitted parameters
