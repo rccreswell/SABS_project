77e5c7ae8c5f42735905d59a95ead309ec6f8799
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2019-11-08 16:56:06 +0000

    convert cellml to myokit

--- sabs_pkpd/load_model.py
+++ sabs_pkpd/load_model.py
@@ -1,4 +1,7 @@
-"""Functions for loading a model from file.
+"""This file contains functions for loading a model from file.
+
+Functionality for parsing CellML models including conversion of stimulus
+protocol) is included.
 """
 
 import myokit
@@ -7,19 +10,72 @@ import myokit.formats
 import matplotlib.pyplot as plt
 
 
-def convert_protocol(stimulus_protocol):
-    """Convert a cellml stimulus protocol to Myokit format.
+def convert_protocol(model):
+    """Convert the cellml stimulus protocol the the correct format for myokit.
+
+    CellML models may contain a stimulus current. Myokit's CellML import does
+    not automatically convert the stimulus current into the Myokit pacing
+    protocol. This function detects the stimulus protocol model component after
+    import from CellML and parses the contents for rewriting in Myokit pacing
+    format.
 
     Parameters
     ----------
-    stimulus_protocol : str
-        The cellml stimulus protocol
+    model : myokit.Model
+        The model as imported from cellml
 
     Returns
     -------
-    protocol...
+    myokit.Model
+        The model with corrected stimulus protocol
+    myokit.Protocol
+        stimulus protocol using Myokit's pacing system
     """
-    pass
+    # Get the original protocol from cellml
+    original_protocol_component = model.get('stimulus_protocol',
+                                            class_filter=myokit.Component)
+
+    # Read the parameters from the original protocol
+    equations = original_protocol_component.equations()
+    pacing_parameters = {}
+    for equation in equations:
+        if equation.lhs.pystr() in ['IstimStart',
+                                    'IstimPeriod',
+                                    'IstimPulseDuration']:
+            pacing_parameters[equation.lhs.pystr()] = equation.rhs
+        if equation.lhs.pystr() == 'IstimAmplitude':
+            amplitude = equation.rhs
+
+    # Add the level variable which is bound to pace
+    level = original_protocol_component.add_variable('level')
+    level.set_rhs(0)
+    level.set_binding('pace')
+
+    # Delete those variables which are no longer needed in the stimulus model
+    # component
+    variables_to_delete = []
+    for variable in original_protocol_component.variables():
+        if variable.name() == 'IstimAmplitude':
+            variable.set_rhs(0.5)
+        elif variable.name() == 'Istim':
+            variable.set_rhs('level * IstimAmplitude')
+        elif variable.name() == 'level':
+            pass
+        else:
+            variables_to_delete.append(variable)
+
+    for variable in variables_to_delete:
+        original_protocol_component.remove_variable(variable)
+
+    # Write the protocol in Myokit format
+    new_protocol = myokit.Protocol()
+    new_protocol.schedule(1.0,
+                          pacing_parameters['IstimStart'],
+                          pacing_parameters['IstimPulseDuration'],
+                          period=pacing_parameters['IstimPeriod'],
+                          multiplier=0)
+
+    return model, new_protocol
 
 
 def load_simulation_from_cellml(filename):
@@ -32,7 +88,7 @@ def load_simulation_from_cellml(filename):
 
     Returns
     -------
-    myokit._sim.cvodesim.Simulation
+    myokit.Simulation
         Myokit Simulation object from the cellml file
     """
     if 'cellml' not in myokit.formats.importers():
@@ -42,63 +98,18 @@ def load_simulation_from_cellml(filename):
     importer = myokit.formats.importer('cellml')
     model = importer.model(filename)
 
-    # p = myokit.Protocol()
-    # s = myokit.Simulation(model, p)
-    #
+    model, protocol = convert_protocol(model)
+    s = myokit.Simulation(model, protocol)
+
+    return s
+
+    # Code for a simulation
     # d = s.run(1000)
     # first_state = next(model.states())
     # var = first_state.qname()
     # plt.plot(d.time(), d[var])
     # plt.title(var)
     # plt.show()
-
-    if model.has_component('stimulus_protocol'):
-        protocol_component = model.get('stimulus_protocol', class_filter=myokit.Component)
-
-        equations = protocol_component.equations()
-        # for eq in equations:
-        #     print(eq.lhs, eq.lhs.pystr(), type(eq.lhs))
-        #     #print(eq.rhs, eq.rhs.unit(), type(eq.rhs))
-
-        # Get the Start, Duration, and Period, which will go in the pacing protocol
-        # Get IstimAmplitude, which is kept as is
-        pacing_parameters = {}
-        amplitude = None
-        for equation in equations:
-            if equation.lhs.pystr() in ['IstimStart', 'IstimPeriod', 'IstimPulseDuration']:
-                pacing_parameters[equation.lhs.pystr()] = equation.rhs
-            if equation.lhs.pystr() == 'IstimAmplitude':
-                amplitude = equation
-
-        new_stimulus_protocol = myokit.Component(model, 'stimulus_protocol')
-        new_protocol = myokit.Protocol()
-
-        # Make the level bind
-        new_amplitude = myokit.Variable(new_stimulus_protocol, 'IstimAmplitude')
-        new_amplitude.set_rhs(amplitude.rhs)
-        #new_amplitude.set_unit()
-        # new_stimulus_protocol.add_variable('IstimAmplitude')
-
-        level = myokit.Variable(new_stimulus_protocol, 'level')
-        level.set_rhs(0)
-        level.set_binding('pace')
-
-        # Make a new Istim =
-        istim = myokit.Variable(new_stimulus_protocol, 'Istim')
-        istim.set_rhs(myokit.Multiply(myokit.Name(level), myokit.Name(new_amplitude)))
-
-        protocol_component = new_stimulus_protocol
-
-        new_protocol.schedule(1.0, pacing_parameters['IstimStart'], pacing_parameters['IstimPulseDuration'], period=pacing_parameters['IstimPeriod'], multiplier=0)
-
-        # s = myokit.Simulation(model, new_protocol)
-        # d = s.run(1000)
-        # first_state = next(model.states())
-        # var = first_state.qname()
-        # plt.plot(d.time(), d[var])
-        # plt.title(var)
-        # plt.show()
-
 
 
 def load_simulation_from_mmt(filename):
@@ -111,14 +122,11 @@ def load_simulation_from_mmt(filename):
 
     Returns
     -------
-    myokit._sim.cvodesim.Simulation
+    myokit.Simulation
         Myokit Simulation object from the MMT file
     """
     model, prot, script = myokit.load(filename)
     return myokit.Simulation(model, prot)
-
-
-
 
 
 if __name__ == '__main__':
