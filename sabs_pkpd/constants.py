456c4386906654c5f37fa8c2bc14cbf33d0bd05d
YannStanislasBarral <56925303+YannStanislasBarral@users.noreply.github.com>
2019-11-14 11:36:06 +0000

    Update constants.py

--- sabs_pkpd/constants.py
+++ sabs_pkpd/constants.py
@@ -2,6 +2,6 @@ import sabs_pkpd
 
 n = 2
 
-s = sabs_pkpd.run_model.set_myokit_simulation('./tests/test resources/pints_problem_def_test.mmt')
+s = []
 
 data_exp = sabs_pkpd.load_data.Data_exp([], [], [], [])
