7dc1c3a945cf7556d4622b7bab2d790758cc2297
me <me@example.com>
2019-11-15 12:35:57 +0000

    added function clamp_experiment_model and associated test

--- sabs_pkpd/clamp_experiment.py
+++ sabs_pkpd/clamp_experiment.py
@@ -18,10 +18,13 @@ def clamp_experiment_model(model_filename, clamped_param_annot:str, protocol=Non
     :param model_filename: str
         Path and filename to the MMT model loaded.
     :param clamped_param_annot: str
-
-    :param protocol:
-    :param save_new_mmt_filename:
-    :return:
+        MMT model annotation for the model variable clamped. This variable's values will be set by the protocol.
+    :param protocol: Myokit.Protocol()
+        If specified by the user, the protocol will be added to the MMT file saved.
+    :param save_new_mmt_filename: str
+        Path and filename to the location where the user wants to save the model ready for clamping simulations.
+    :return: m: Myokit.Model()
+        Returns a Myokit.Model() ready to be used with a clamping protocol to generate a Myokit.Simulation().
     """
     if protocol is not None:
         m = myokit.load_model(model_filename)
@@ -37,66 +40,20 @@ def clamp_experiment_model(model_filename, clamped_param_annot:str, protocol=Non
     original_protocol_component = m.get(component_name,
                                                 class_filter=myokit.Component)
 
+    variable_found = False
     for variable in original_protocol_component.variables():
         if variable.name() == variable_name:
             variable.set_rhs('engine.pace')
+            variable_found = True
+
+    if variable_found == False:
+        raise ValueError('The variable ' + clamped_param_annot + 'could not be found.')
 
     # Save the new model and protocol if the user provided the argument save_new_mmt_filename
     if save_new_mmt_filename is not None:
-        myokit.save(save_new_mmt_filename, model=m, protocol=protocol)
+        if protocol is not None:
+            myokit.save(save_new_mmt_filename, model=m, protocol=protocol)
+        else:
+            myokit.save(save_new_mmt_filename, model=m)
 
     return m
-
-
-    # Run the new model
-    s = myokit.Simulation(m, p)
-    s.reset()
-    # reset timer
-    s.set_time(0)
-
-    a = s.run(time_max, log_times=time_samples)
-    output = a[read_out]
-
-
-
-modelname = 'C:/Users/yanral/Documents/Software Development/tests/test resources/pints_problem_def_test.mmt'
-# In[]
-m, p, script = myokit.load(modelname)
-
-
-# Design the clamp protocol
-time_max = 30
-n_timepoints = 100
-time_samples = np.linspace(0, time_max, n_timepoints)
-read_out = 'comp1.x'
-exp_clamped_parameter_annot = 'comp1.y'
-exp_clamped_parameter_values = 1 + np.sin(time_samples)
-
-p = myokit.Protocol()
-for i in range(len(time_samples) - 1):
-    p.schedule(exp_clamped_parameter_values[i], time_samples[i], time_samples[i + 1] - time_samples[i])
-
-
-# Change the model to clamp the selected value
-original_protocol_component = m.get('comp1',
-                                            class_filter=myokit.Component)
-for variable in original_protocol_component.variables():
-    if variable.name() == 'y':
-        variable.set_rhs('engine.pace')
-
-
-# Save the new model and protocol if the user provided the argument save_new_mmt_filename
-newmodelname = 'C:/Users/yanral/Documents/Software Development/tests/test resources/model_clamped.mmt'
-myokit.save(newmodelname, model=m, protocol=p)
-
-
-# Run the new model
-s = myokit.Simulation(m, p)
-s.reset()
-# reset timer
-s.set_time(0)
-
-a = s.run(time_max, log_times=time_samples)
-output = a[read_out]
-
-
---
+++ tests/test_clamp_experiment.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 11 15:06:09 2019
+
+@author: yanral
+"""
+import sabs_pkpd
+import numpy as np
+import matplotlib.pyplot as plt
+import myokit
+
+
+def test_clamp_experiment_model():
+    modelname = 'C:/Users/yanral/Documents/Software Development/tests/test resources/pints_problem_def_test.mmt'
+
+    # Design the clamp protocol
+    time_max = 30
+    n_timepoints = 10
+    time_samples = np.linspace(2, time_max, n_timepoints)
+    read_out = 'comp1.x'
+    exp_clamped_parameter_annot = 'comp1.y'
+    exp_clamped_parameter_values = 1 + np.sin(time_samples)
+
+    p = myokit.Protocol()
+    for i in range(len(time_samples) - 1):
+        p.schedule(exp_clamped_parameter_values[i], time_samples[i], time_samples[i + 1] - time_samples[i])
+
+    # Save the new model and protocol if the user provided the argument save_new_mmt_filename
+    newmodelname = 'C:/Users/yanral/Documents/Software Development/tests/test resources/model_clamped.mmt'
+
+    m = sabs_pkpd.clamp_experiment.clamp_experiment_model(modelname, exp_clamped_parameter_annot, p, newmodelname)
+
+    s = sabs_pkpd.load_model.load_simulation_from_mmt(newmodelname)
+
+    s.reset()
+    # reset timer
+    s.set_time(0)
+
+    a = s.run(time_max, log_times=time_samples)
+    output = a[read_out]
+
+    expected_output = np.array([0.00901, 0.06237, 0.06472, 0.11874, 0.12048, 0.17507, 0.17629, 0.23134, 0.23215])
+    diff = np.linalg.norm(output-expected_output)
+
+    assert diff < 0.0001
