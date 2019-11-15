28ea78032af678f9d7b095600d122868e6d4048e
me <me@example.com>
2019-11-15 10:58:27 +0000

    commented convert_protocol test

--- sabs_pkpd/clamp_experiment.py
+++ sabs_pkpd/clamp_experiment.py
@@ -14,9 +14,8 @@ modelname = 'C:/Users/yanral/Documents/Software Development/tests/test resources
 # In[]
 m, p, script = myokit.load(modelname)
 
-s = myokit.Simulation(m, p)
 
-# In[]
+# Design the clamp protocol
 time_max = 30
 n_timepoints = 100
 time_samples = np.linspace(0, time_max, n_timepoints)
@@ -24,16 +23,26 @@ read_out = 'comp1.x'
 exp_clamped_parameter_annot = 'comp1.y'
 exp_clamped_parameter_values = 1 + np.sin(time_samples)
 
-plt.plot(exp_clamped_parameter_values)
-plt.title('Clamped value ' + exp_clamped_parameter_annot)
-
 p = myokit.Protocol()
-
 for i in range(len(time_samples) - 1):
     p.schedule(exp_clamped_parameter_values[i], time_samples[i], time_samples[i + 1] - time_samples[i])
 
-myokit.save(modelname, model=m, protocol=p)
 
+# Change the model to clamp the selected value
+original_protocol_component = m.get('comp1',
+                                            class_filter=myokit.Component)
+for variable in original_protocol_component.variables():
+    if variable.name() == 'y':
+        variable.set_rhs(0)
+
+
+# Save the new model and protocol
+newmodelname = 'C:/Users/yanral/Documents/Software Development/tests/test resources/model_clamped.mmt'
+myokit.save(newmodelname, model=m, protocol=p)
+
+
+# Run the new model
+s = myokit.Simulation(m, p)
 s.reset()
 # reset timer
 s.set_time(0)
