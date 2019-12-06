23d129368595a1e70bf0e1b341c3411f903a71c4
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2019-12-06 18:26:10 +0000

    further work on particle swarm, parallelization, and model parameter updates

--- sabs_pkpd/optimize_protocol.py
+++ sabs_pkpd/optimize_protocol.py
@@ -3,8 +3,6 @@
 (In Progress)
 # TODO: generalize noise (currently it is Gaussian with known, fixed sigma)
 # TODO: add model parameter priors
-# TODO: better optimization algorithm for protocol parameters?
-# TODO: speed up the code
 
 Current structure of the code:
 
@@ -27,7 +25,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pints
 import pints.plot
-
+from multiprocessing import Pool
+import os
 
 class ProtocolOptimizer:
     """Handles optimization of the protocol for parameter inference.
@@ -39,7 +38,8 @@ class ProtocolOptimizer:
                  x0,
                  model_params,
                  protocol_params,
-                 num_mcmc_iterations):
+                 num_mcmc_iterations,
+                 true_model_params=None):
         """
         Parameters
         ----------
@@ -59,11 +59,16 @@ class ProtocolOptimizer:
         x0 : float
             The value x(time=0)
         model_params : list of float
-            The initial/ground truth values of the model parameters. Must be
-            compatible with simulator
+            The initial values of the model parameters. Must be
+            compatible with simulator. These are used to evaluate the objective
+            function.
         protocol_params : list of float
             The initial values of the protocol parameters. Must be compatible
             with protocol_form
+        true_model_params : list of float, optional
+            The ground truth model parameters. If not supplied, set equal to
+            model_params. These are used for synthetic data generation and
+            plotting.
         """
         self.times = times
         self.x0 = x0
@@ -73,6 +78,11 @@ class ProtocolOptimizer:
         self.protocol_params = protocol_params
 
         self.num_mcmc_iterations = num_mcmc_iterations
+
+        if true_model_params is not None:
+            self.true_model_params = true_model_params
+        else:
+            self.true_model_params = self.model_params.copy()
 
 
     def run_original_protocol(self):
@@ -107,12 +117,24 @@ class ProtocolOptimizer:
         """
         # If a matrix of protocol params is supplied, evaluate each one sequentially
         # and return a list of objectives
+
+        # if type(protocol_params) is np.ndarray:
+        #     if protocol_params.ndim == 2:
+        #         all_F = []
+        #         for p in protocol_params:
+        #             all_F.append(self.objective(p))
+        #         return np.array(all_F)
+
         if type(protocol_params) is np.ndarray:
             if protocol_params.ndim == 2:
-                all_F = []
-                for p in protocol_params:
-                    all_F.append(self.objective(p))
-                return np.array(all_F)
+                pool = Pool(os.cpu_count())
+                all_objectives = pool.map(self.objective, [p for p in protocol_params])
+
+                pool.close()
+                pool.join()
+
+                return np.array(all_objectives)
+
 
         # Get the protocol as a function of t
         protocol = self.protocol_form(*protocol_params)
@@ -148,6 +170,9 @@ class ProtocolOptimizer:
 
         print(protocol_params)
 
+        # return -np.trace(J)
+        # return -np.linalg.det(J)
+
         # Calculate the objective function from the CRLBs
         try:
             J_inv = np.linalg.inv(J)
@@ -158,13 +183,40 @@ class ProtocolOptimizer:
         F = np.sum([J_inv[i,i] / self.model_params[i] \
                     for i in range(len(self.model_params))])
 
+        # Try regularizing the objective??? Could help avoid exploding, if it makes sense.
+        F += 0.001 * np.sum(np.array(protocol_params)**2)
+
         return F
 
 
     def optimize_protocol(self):
         """Optimize the protocol parameters to maximize the Fisher objective.
         """
+        import pyswarms as ps
+
         objective = lambda protocol_params : self.objective(protocol_params)
+
+        num_particles = 25
+        options = {'c1': 0.9, 'c2': 0.2, 'w':0.9}
+        dim = len(self.protocol_params)
+
+        # Start one particle at the baseline protocol. Start all the others
+        # at randomly generated points about it
+        init_pos = [self.protocol_params]
+        for _ in range(num_particles-1):
+            init_pos.append(np.random.normal(self.protocol_params, np.abs(self.protocol_params)*5))
+        init_pos = np.array(init_pos)
+
+        optimizer = ps.single.GlobalBestPSO(n_particles=num_particles, dimensions=dim, options=options, init_pos=init_pos)
+        cost, pos = optimizer.optimize(objective, iters=100)
+        print(cost)
+        print(pos)
+        protocol = pos
+
+        self.protocol_params = protocol
+
+
+        # Other optimizers to try...
 
         # res = scipy.optimize.minimize(
         #           objective,
@@ -182,17 +234,11 @@ class ProtocolOptimizer:
         # protocol = res.x
         # print('optimized_protocol', protocol)
 
-
-        import pyswarms as ps
-        options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
-        optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=6, options=options)
-        cost, pos = optimizer.optimize(objective, iters=20)
-        print(cost)
-        print(pos)
-        protocol = pos
-
-
-        self.protocol_params = protocol
+        # import cma
+        # es = cma.CMAEvolutionStrategy(self.protocol_params, 0.5)
+        # res = es.optimize(objective)
+        # print(res)
+        # exit()
 
 
     def infer_model_parameters(self):
@@ -202,13 +248,29 @@ class ProtocolOptimizer:
                             self.simulator,
                             self.x0,
                             self.protocol_form(*self.protocol_params),
-                            self.model_params,
+                            self.true_model_params,
                             self.times,
                             self.num_mcmc_iterations
                          )
 
         self.data = values
         self.posterior = chains
+
+
+    def update_model_parameters(self):
+        """Set the model parameters to the current posterior medians.
+
+        Requires that self.posterior be populated with the desired MCMC
+        samples, by running self.infer_model_parameters() first.
+        """
+        burnin = self.num_mcmc_iterations // 2
+
+        for i in range(len(self.model_params)):
+            posterior = self.posterior[0,:,i][burnin:]
+            self.model_params[i] = np.median(posterior)
+
+        print('reset to:')
+        print(self.model_params)
 
 
     def plot(self):
@@ -230,7 +292,9 @@ class ProtocolOptimizer:
         axes[0,1].set_title('data from initial protocol')
 
         for i, model_param in enumerate(self.model_params):
-            axes[0,i+2].hist(self.original_posterior[0,:,i][burnin:])
+            true_value = self.true_model_params[i]
+            axes[0,i+2].hist(self.original_posterior[0,:,i][burnin:], alpha=0.8)
+            axes[0,i+2].axvline(true_value, zorder=-10, color='mediumseagreen')
             axes[0,i+2].set_title('parameter {}'.format(i+1))
 
         # Optimized protocol
@@ -242,7 +306,9 @@ class ProtocolOptimizer:
         axes[1,1].set_title('data from optimized protocol')
 
         for i, model_param in enumerate(self.model_params):
-            axes[1,i+2].hist(self.posterior[0,:,i][burnin:])
+            true_value = self.true_model_params[i]
+            axes[1,i+2].hist(self.posterior[0,:,i][burnin:], alpha=0.8)
+            axes[1,i+2].axvline(true_value, zorder=-10, color='mediumseagreen')
             axes[1,i+2].set_title('parameter {}'.format(i+1))
 
         fig.set_tight_layout(True)
@@ -295,7 +361,7 @@ def infer_model_parameters(simulator,
     problem = pints.SingleOutputProblem(m, times, values)
     likelihood = pints.GaussianKnownSigmaLogLikelihood(problem, 0.1)
 
-    prior = pints.UniformLogPrior([-100.0]*len(true_model_params),
+    prior = pints.UniformLogPrior([0.0]*len(true_model_params),
                                   [100.0]*len(true_model_params))
 
     log_posterior = pints.LogPosterior(likelihood, prior)
@@ -322,6 +388,9 @@ def one_step_protocol(amplitude, duration):
 def sine_wave_protocol(amplitude, frequency):
     return lambda times : amplitude * np.sin(frequency * times)
 
+def three_sine_wave_protocol(a1, a2, a3, f1, f2, f3):
+    return lambda times : a1 * np.sin(f1*times) + a2 * np.sin(f2*times) + a3 * np.sin(f3*times)
+
 def three_event_protocol(d1, d2, d3, a1, a2, a3):
     def f(t):
         if type(t) is float or type(t) is np.float64:
@@ -336,6 +405,36 @@ def three_event_protocol(d1, d2, d3, a1, a2, a3):
                 (l1 * ((t > 1) & (t < 1+d1))) + \
                 (l2 * ((t > 1+d1) & (t < 1+d1+d2))) + \
                 (l3 * ((t > 1+d1+d2) & (t < 1 +d1+d2+d3)))
+
+    return f
+
+import scipy.interpolate
+def general_protocol(*values):
+    def f(t):
+        lined_up_times = np.linspace(0, 10, len(values))
+        return scipy.interpolate.interp1d(lined_up_times, values)(t)
+
+    return f
+
+
+def five_event_protocol(d1, d2, d3, d4, d5, a1, a2, a3, a4, a5):
+    def f(t):
+        if type(t) is float or type(t) is np.float64:
+            t = np.array([t])
+
+        baseline = np.zeros(len(t))
+        l1 = np.ones(len(t)) * a1
+        l2 = np.ones(len(t)) * a2
+        l3 = np.ones(len(t)) * a3
+        l4 = np.ones(len(t)) * a4
+        l5 = np.ones(len(t)) * a5
+
+        return baseline + \
+                (l1 * ((t > 1) & (t < 1+d1))) + \
+                (l2 * ((t > 1+d1) & (t < 1+d1+d2))) + \
+                (l3 * ((t > 1+d1+d2) & (t < 1 +d1+d2+d3))) + \
+                (l4 * ((t > 1+d1+d2+d3) & (t < 1 +d1+d2+d3+d4))) + \
+                (l5 * ((t > 1+d1+d2+d3+d4) & (t < 1 +d1+d2+d3+d4+d5)))
 
     return f
 
@@ -395,20 +494,21 @@ def damped_harmonic_oscillator(c, k, m, beta, protocol, t, x0):
     """
     # print(c, k, m, beta)
     m = 1e-1 if m < 0 else m
-    beta = 0 if beta < 0 else beta
+    # beta = 0 if beta < 0 else beta
     c = 0 if c < 0 else c
     k = 0 if k < 0 else k
 
     def f(t, x):
         d = [0, 0]
         d[0] = x[1]
-        d[1] = 1/m * protocol(t) - c/m * x[1] -k/m * x[0] - beta * x[0]**3
+        d[1] = 1/m * protocol(t) - c/m * x[1] -k/m * x[0] - beta/m * x[0]**3
         return d
 
     result = scipy.integrate.solve_ivp(
             f, (min(t), max(t)), [x0, 0], t_eval=t, max_step=0.1, vectorized=True).y[0]
 
     return result
+
 
 def main():
 
@@ -428,17 +528,21 @@ def main():
 
     if True:
         opt = ProtocolOptimizer(damped_harmonic_oscillator,
-                               three_event_protocol,
-                               np.linspace(0, 10, 1000),
+                               five_event_protocol,
+                               np.linspace(0, 10, 400),
                                0.0,
-                               [10, 1.0, 1.0, 1.0],
-                               [1, 1, 1, 1, 2, 3],
-                               4000)
-
-        opt.run_original_protocol()
-        opt.optimize_protocol()
-        opt.infer_model_parameters()
-        opt.plot()
+                               [5.0, 10.0, 1.0, 10.0],
+                               # [.2, .2, .2, .2, .2, -5.5, -5.5, -5.5, -5.5, -5.5],
+                               [1.70015678e+00, 1.92681600e+00, -1.12329275e-01, 9.86565896e-01, 4.02746746e-01, 1.96004128e+02, 7.16528663e+01, -2.13426171e+01, 1.32499357e+01, -4.06159185e+01],
+                               # [0.0, 20.0, 20.0, 20.0, 20.0] + [0.0] * 20,
+                               4000,
+                               true_model_params=[5.0, 10.0, 1.0, 10.0])
+        for _ in range(1):
+            opt.run_original_protocol()
+            opt.optimize_protocol()
+            opt.infer_model_parameters()
+            opt.plot()
+            opt.update_model_parameters()
 
     if False:
         opt = ProtocolOptimizer(logistic_growth_additive_protocol,
