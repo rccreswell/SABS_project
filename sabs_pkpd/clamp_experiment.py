fd91b04eccef094b6649a2828b08be734b5971c5
me <me@example.com>
2019-12-13 16:21:43 +0000

    added tests for get_steady_state

--- sabs_pkpd/clamp_experiment.py
+++ sabs_pkpd/clamp_experiment.py
@@ -7,8 +7,135 @@ Created on Mon Nov 11 15:06:09 2019
 
 import sabs_pkpd
 import numpy as np
-import matplotlib.pyplot as plt
 import myokit
+import os
+
+
+def get_steady_state(s, time_to_steady_state, data_exp=None, save_location=None, list_of_models_names =None):
+    """
+    This function returns the (pseudo-)steady-state of the models provided. The output of the function is a list of
+    length the number of models, and each index contains the number of different experimental conditions provided with
+    data_exp.
+
+    :param s:
+    myokit.Simulation or list of myokit.Simulation. Contains the model(s) for which the steady-state will be computed.
+
+    :param data_exp:
+    sabs_pkpd.load_data.Data_exp. Contains the experimental conditions that the user wants to use to compute different
+    steady-states. If not provided, the steady-state is also directly saved as default state of the model in s.
+
+    :param time_to_steady_state:
+    float. Time at which to read out
+
+    :return: steady-state
+    list. List of length the number of models, and each index contains the number of different experimental conditions
+    provided with data_exp. Refer to steady_state[i][j] for the i-th model steady-state under j-th experimental
+    conditions
+    """
+
+    # test types of variables provided
+    if data_exp is not None:
+        if type(data_exp) != sabs_pkpd.load_data.Data_exp:
+            raise ValueError('data_exp should be provided as a class sabs_pkpd.load_data.Data_exp.')
+
+    if type(s) != myokit._sim.cvodesim.Simulation:
+        if type(s) != list:
+            raise ValueError('s should be provided as a myokit.Simulation or list of myokit.Simulation')
+        else:
+            for j in range(len(s)):
+                if type(s[j]) != myokit._sim.cvodesim.Simulation:
+                    raise ValueError('s should be provided as a myokit.Simulation or list of myokit.Simulation')
+
+    if data_exp is not None:
+        if not hasattr(data_exp, 'fitting_instructions'):
+            raise ValueError('data_exp should contain the fitting_instructions attribute (at least exp_cond_param_annot)'
+                             'to allow set up of the experimental conditions to compute steady-state')
+
+    # Distinguish one or more different models
+    if type(s) == list:
+        if data_exp is None:
+            steady_state = []
+            for i in range(len(s)):
+                s[i].run(time_to_steady_state)
+                steady_state.append([s[i].state()])
+                s[i].set_default_state(steady_state[i][0])
+
+        else:
+            steady_state = []
+            for i in range(len(s)):
+                model_ss = []
+                for j in range(len(data_exp.exp_conds)):
+                    s[i].reset()
+                    s[i].set_constant(data_exp.fitting_instructions.exp_cond_param_annot, data_exp.exp_conds[j])
+                    s[i].run(time_to_steady_state)
+                    model_ss.append(s[i].state())
+                steady_state.append(model_ss)
+
+    else:
+        if data_exp is None:
+            s.run(time_to_steady_state)
+            steady_state = [[s.state()]]
+            s.set_default_state(steady_state[0][0])
+        else:
+            steady_state = [[]]
+            for i in range(len(data_exp.exp_conds)):
+                s.reset()
+                s.set_constant(data_exp.fitting_instructions.exp_cond_param_annot, data_exp.exp_conds[i])
+                s.run(time_to_steady_state)
+                steady_state[0].append(s.state())
+
+    if save_location is not None:
+        if list_of_models_names is None:
+            if hasattr(sabs_pkpd.constants, 'protocol_optimisation_instructions'):
+                list_of_models_names = sabs_pkpd.constants.protocol_optimisation_instructions.models
+            else:
+                list_of_models_names = []
+                for i in range(len(steady_state)):
+                    list_of_models_names.append('model ' + str(i))
+            save_steady_state_to_mmt(s, steady_state, save_location, list_of_models_names)
+
+    return steady_state
+
+
+def save_steady_state_to_mmt(s, steady_state, save_location, list_of_models_names):
+    """
+
+
+    :param s:
+    myokit.Simulation or list of myokit.Simulation. Contains the model(s) for which the steady-state will be computed.
+
+    :param steady-state:
+    list. List of length the number of models, and each index contains the number of different experimental conditions
+    provided with data_exp.
+
+    :param save_filename:
+    string. Filename for the location where to save the new model produced. If more than one model is provided,
+    sub-directories matching the models names in list_of_models_names will be created, and a .mmt file created for each
+    .
+
+    :param list_of_models_names:
+
+    :return:
+    """
+
+    if len(steady_state) != list_of_models_names:
+        raise ValueError('Steady_state and list_of_models_names should have the same length.')
+
+    if len(steady_state) == 1:
+        if len(steady_state[0]) == 1:
+            s.set_default_state(steady_state[0])
+            if save_location[-4:] != '.mmt':
+                save_filename = save_location + '.mmt'
+
+            save_location = save_location + '.mmt'
+        if not os.path.exists(save_location):
+            os.makedirs(save_location)
+            os.path.join('C:', 'Documents')
+
+    else:
+        pass
+    return None
+
 
 def clamp_experiment_model(model_filename, clamped_variable_annot:str, pace_variable_annotation:str, protocol=None, save_new_mmt_filename=None):
     """
