082ae66f56344417f89145ea0a27bd724ace8b93
rcw5890 <richard.creswell@hertford.ox.ac.uk>
2020-07-02 19:18:49 +0100

    style run_model.py

--- sabs_pkpd/run_model.py
+++ sabs_pkpd/run_model.py
@@ -6,27 +6,35 @@ import matplotlib.pyplot as plt
 
 def simulate_data(fitted_params_values, s, data_exp, pre_run=0):
     """
-    This function runs the model in the same conditions (and with the same time sampling) as the experimental data
-    loaded in data_exp.
+    This function runs the model in the same conditions (and with the same time
+    sampling) as the experimental data loaded in data_exp.
+
     :param fitted_params_values: list
-        List of the values for the fitted parameters. It has to match the length of fitting parameters annotations.
+        List of the values for the fitted parameters. It has to match the
+        length of fitting parameters annotations.
     :param s: Myokit.Simulation
         Myokit simulation defined by the chosen model and protocol.
     :param data_exp: Data_exp
-        Contains the data that the model is fitted too. See documentation for sabs_pkpd.load_data for further info
+        Contains the data that the model is fitted too. See documentation for
+        sabs_pkpd.load_data for further info
     :param pre_run: int
-        Defines the time for which the model is run without returning output. Useful for reaching (quasi-) steady-state
+        Defines the time for which the model is run without returning output.
+        Useful for reaching (quasi-) steady-state
     :return: output : list
-        List of the same shape as data_exp.times. It contains the model output in the given conditions at the time
-        points used to generate the experimental data.
+        List of the same shape as data_exp.times. It contains the model output
+        in the given conditions at the time points used to generate the
+        experimental data.
     """
 
     # Allocate memory for the output
     output = []
 
-    # Verify that the parameters for fitting and their values have the same length
-    if len(fitted_params_values) != len(data_exp.fitting_instructions.fitted_params_annot):
-        raise ValueError('Fitted parameters annotations and values should have the same length')
+    # Verify that the parameters for fitting and their values have the same
+    # length
+    if len(fitted_params_values) != \
+            len(data_exp.fitting_instructions.fitted_params_annot):
+        raise ValueError('Fitted parameters annotations and values should '
+                         'have the same length')
 
     # Run the model solving for all experiment conditions
     for k in range(0, len(set(data_exp.exp_conds))):
@@ -40,24 +48,32 @@ def simulate_data(fitted_params_values, s, data_exp, pre_run=0):
         else:
             state_to_set = sabs_pkpd.constants.s.state()
 
-        for i in range(0, len(data_exp.fitting_instructions.fitted_params_annot)):
-            if sabs_pkpd.pints_problem_def.parameter_is_state(data_exp.fitting_instructions.fitted_params_annot[i],
-                                                              sabs_pkpd.constants.s):
-                index = sabs_pkpd.pints_problem_def.find_index_of_state(data_exp.fitting_instructions.fitted_params_annot[i],
-                                                                        sabs_pkpd.constants.s)
+        for i in range(0,
+                       len(data_exp.fitting_instructions.fitted_params_annot)):
+            if sabs_pkpd.pints_problem_def.parameter_is_state(
+                    data_exp.fitting_instructions.fitted_params_annot[i],
+                    sabs_pkpd.constants.s):
+                index = sabs_pkpd.pints_problem_def.find_index_of_state(
+                    data_exp.fitting_instructions.fitted_params_annot[i],
+                    sabs_pkpd.constants.s)
                 state_to_set[index] = fitted_params_values[i]
 
         # Set the value of the initial state to what it should be
         sabs_pkpd.constants.s.set_state(state_to_set)
 
         # Set constant parameters values.
-        for i in range(0, len(data_exp.fitting_instructions.fitted_params_annot)):
-            if not sabs_pkpd.pints_problem_def.parameter_is_state(data_exp.fitting_instructions.fitted_params_annot[i],
-                                                                  sabs_pkpd.constants.s):
-                s.set_constant(data_exp.fitting_instructions.fitted_params_annot[i], fitted_params_values[i])
+        for i in range(0,
+                       len(data_exp.fitting_instructions.fitted_params_annot)):
+            if not sabs_pkpd.pints_problem_def.parameter_is_state(
+                    data_exp.fitting_instructions.fitted_params_annot[i],
+                    sabs_pkpd.constants.s):
+                s.set_constant(
+                    data_exp.fitting_instructions.fitted_params_annot[i],
+                    fitted_params_values[i])
 
         # set the right experimental conditions
-        s.set_constant(data_exp.fitting_instructions.exp_cond_param_annot, list(data_exp.exp_conds)[k])
+        s.set_constant(data_exp.fitting_instructions.exp_cond_param_annot,
+                       list(data_exp.exp_conds)[k])
 
         # Eventually run a pre-run to reach steady-state
         s.pre(pre_run)
@@ -66,18 +82,29 @@ def simulate_data(fitted_params_values, s, data_exp, pre_run=0):
         a = s.run(data_exp.times[k][-1]*1.00001, log_times=data_exp.times[k])
 
         # Convert output in concentration
-        output.append(list(a[data_exp.fitting_instructions.sim_output_param_annot]))
+        output.append(
+                list(a[data_exp.fitting_instructions.sim_output_param_annot]))
 
     return output
 
 
-def quick_simulate(s, time_max, read_out: str,  exp_cond_param_annot=None, exp_cond_param_values=None,
-                   fixed_params_annot=None, fixed_params_values=None, pre_run=0, time_samples=None):
+def quick_simulate(s,
+                   time_max,
+                   read_out: str,
+                   exp_cond_param_annot=None,
+                   exp_cond_param_values=None,
+                   fixed_params_annot=None,
+                   fixed_params_values=None,
+                   pre_run=0,
+                   time_samples=None):
 
     """
     This function returns a simulation for any desired conditions.
-    Note that s is not reloaded here, meaning that if you previously set a constant in a previous simulation, this will
-    not be changed until you either reset manually the constant's value (s.set_constant()) or reload the mmt model.
+
+    Note that s is not reloaded here, meaning that if you previously set a
+    constant in a previous simulation, this will not be changed until you
+    either reset manually the constant's value (s.set_constant()) or reload the
+    mmt model.
 
     :param s: Myokit.Simulation
         Myokit simulation defined by the chosen model and protocol.
@@ -86,52 +113,63 @@ def quick_simulate(s, time_max, read_out: str,  exp_cond_param_annot=None, exp_c
         Maximal time for which the model is run
 
     :param read_out: str
-        MMT model annotation of the variable read out as output from the model simulation.
+        MMT model annotation of the variable read out as output from the model
+        simulation.
 
     :param exp_cond_param_annot: str
-        MMT model annotation of the experimental condition varying when generating the data.
+        MMT model annotation of the experimental condition varying when
+        generating the data.
 
     :param exp_cond_param_values: list
-        List of values for the experimental condition in which the model should be run. The model solving is looped over
-        the experimental condition values (1 run per value).
+        List of values for the experimental condition in which the model should
+        be run. The model solving is looped over the experimental condition
+        values (1 run per value).
 
     :param fixed_params_annot: list of str
-        List of the MMT model annotations of the constants set for the simulation.
+        List of the MMT model annotations of the constants set for the
+        simulation.
 
     :param fixed_params_values: list
         List of values for the constants set for the simulation.
 
     :param pre_run: int
-        Defines the time for which the model is run without returning output. Useful for reaching (quasi-) steady-state
+        Defines the time for which the model is run without returning output.
+        Useful for reaching (quasi-) steady-state
 
     :param time_samples: list
         time points for which the model output is returned.
 
     :return: output : list
-        List of shape (len(experimental condition values), time_samples). It contains the model output in the given
-        conditions at the time points used to generate the experimental data.
+        List of shape (len(experimental condition values), time_samples). It
+        contains the model output in the given conditions at the time points
+        used to generate the experimental data.
     """
     if sabs_pkpd.constants.default_state is None:
-        print('No default state was provided in sabs_pkpd.constants.default_state. Simulating with initial conditions' +
-              ' provided with the .mmt model...')
+        print('No default state was provided in '
+              'sabs_pkpd.constants.default_state. Simulating with initial '
+              'conditions provided with the .mmt model...')
 
     if time_samples is not None:
         if time_samples[-1] > time_max:
-            raise ValueError('The time samples have to be within the range (0 , time_max)')
+            raise ValueError('The time samples have to be within the range '
+                             '(0 , time_max)')
 
     if fixed_params_values is not None or fixed_params_annot is not None:
         if len(fixed_params_annot) != len(fixed_params_values):
-            raise ValueError('The parameters clamped for the simulation must have the same length for names' +
-                             ' and values')
+            raise ValueError('The parameters clamped for the simulation must '
+                             'have the same length for names and values')
 
     if exp_cond_param_values is not None or exp_cond_param_annot is not None:
-        if (not isinstance(exp_cond_param_values, list) and not isinstance(exp_cond_param_values, np.ndarray)) or \
+        if (not isinstance(exp_cond_param_values, list) and not
+                isinstance(exp_cond_param_values, np.ndarray)) or \
                 not isinstance(exp_cond_param_annot, str):
-            raise ValueError('The experimental conditions must be provided as a list or numpy.ndarray, and the ' +
-                             'parameter annotation must be a string matching with the variable name in the MMT model')
+            raise ValueError('The experimental conditions must be provided as '
+                             'a list or numpy.ndarray, and the parameter '
+                             'annotation must be a string matching with the '
+                             'variable name in the MMT model')
         if len(fixed_params_annot) != len(fixed_params_values):
-            raise ValueError('The parameters clamped for the simulation must have the same length for names' +
-                             ' and values')
+            raise ValueError('The parameters clamped for the simulation must '
+                             'have the same length for names and values')
 
     if time_samples is None:
         time_samples = np.linspace(0, time_max, 100)
@@ -154,8 +192,10 @@ def quick_simulate(s, time_max, read_out: str,  exp_cond_param_annot=None, exp_c
                 state_to_set = s.state()
             if fixed_params_annot is not None:
                 for i, annot in enumerate(fixed_params_annot):
-                    if sabs_pkpd.pints_problem_def.parameter_is_state(annot, s):
-                        index = sabs_pkpd.pints_problem_def.find_index_of_state(annot, s)
+                    if sabs_pkpd.pints_problem_def.\
+                            parameter_is_state(annot, s):
+                        index = sabs_pkpd.pints_problem_def.\
+                                    find_index_of_state(annot, s)
                         state_to_set[index] = fixed_params_values[i]
                     else:
                         s.set_constant(annot, fixed_params_values[i])
@@ -187,12 +227,14 @@ def quick_simulate(s, time_max, read_out: str,  exp_cond_param_annot=None, exp_c
         if fixed_params_annot is not None:
             for i, annot in enumerate(fixed_params_annot):
                 if sabs_pkpd.pints_problem_def.parameter_is_state(annot, s):
-                    index = sabs_pkpd.pints_problem_def.find_index_of_state(annot, s)
+                    index = sabs_pkpd.pints_problem_def.\
+                                find_index_of_state(annot, s)
                     state_to_set[index] = fixed_params_values[i]
                 else:
                     s.set_constant(annot, fixed_params_values[i])
 
-        # Set the state to the value corresponding to the eventual clamp or exp condition
+        # Set the state to the value corresponding to the eventual clamp or exp
+        # condition
         s.set_state(state_to_set)
 
         # Eventually run a pre-run to reach steady-state
@@ -206,19 +248,27 @@ def quick_simulate(s, time_max, read_out: str,  exp_cond_param_annot=None, exp_c
     return output
 
 
-def plot_model_vs_data(plotting_parameters_annot, plotting_parameters_values, data_exp, s, pre_run=0, figsize=(20, 20)):
+def plot_model_vs_data(plotting_parameters_annot,
+                       plotting_parameters_values,
+                       data_exp,
+                       s,
+                       pre_run=0,
+                       figsize=(20, 20)):
     """
-    This function plots the experimental data and the output of the model for parameters rescaled as precised by the
-    user
+    This function plots the experimental data and the output of the model for
+    parameters rescaled as precised by the user
 
     :param plotting_parameters_annot:
-    List of strings. Model annotations for the parameters the user wants to rescale for the plotting
+    List of strings. Model annotations for the parameters the user wants to
+    rescale for the plotting
 
     :param plotting_parameters_values:
-    List or numpy.array. Contains the values for the parameters annotated with plotting_params_annot
+    List or numpy.array. Contains the values for the parameters annotated with
+    plotting_params_annot
 
     :param data_exp:
-    Data_exp class. Contains all the experimental data. The fitting instructions must have been initialised previously.
+    Data_exp class. Contains all the experimental data. The fitting
+    instructions must have been initialised previously.
 
     :param s:
     myokit.Simulation. myokit object used to run the model.
@@ -227,7 +277,8 @@ def plot_model_vs_data(plotting_parameters_annot, plotting_parameters_values, da
     Length of pre-run to apply to the model to reach steady-state
 
     :param figsize:
-    Tuple. Defines the size of the figure for the plots. If not specified, it is (20, 20)
+    Tuple. Defines the size of the figure for the plots. If not specified, it
+    is (20, 20)
 
     :return: None
 
@@ -246,7 +297,8 @@ def plot_model_vs_data(plotting_parameters_annot, plotting_parameters_values, da
         number_of_rows = 1
         fig1 = plt.figure()
     else:
-        number_of_rows = number_of_plots//2 + number_of_plots % (number_of_plots//2)
+        number_of_rows = number_of_plots//2 \
+            + number_of_plots % (number_of_plots//2)
         fig1, axes = plt.subplots(number_of_rows, 2, figsize=figsize)
 
     for i in range(0, number_of_plots):
@@ -261,14 +313,23 @@ def plot_model_vs_data(plotting_parameters_annot, plotting_parameters_values, da
         exp_cond_param_values = [data_exp.exp_conds[i]]
         fixed_params_values.append(data_exp.exp_conds[i])
 
-        # Use quick_simulate to generate the model output corresponding to the data
-        sim_data = quick_simulate(s, time_max, read_out,  exp_cond_param_annot, exp_cond_param_values,
-                                  fixed_params_annot, fixed_params_values, time_samples=time_samples, pre_run=pre_run)
+        # Use quick_simulate to generate the model output corresponding to the
+        # data
+        sim_data = quick_simulate(s,
+                                  time_max,
+                                  read_out,
+                                  exp_cond_param_annot,
+                                  exp_cond_param_values,
+                                  fixed_params_annot,
+                                  fixed_params_values,
+                                  time_samples=time_samples,
+                                  pre_run=pre_run)
 
         # Plot the results
         plt.plot(time_samples, sim_data[0], label='Simulated values')
         plt.plot(time_samples, data_exp.values[i], label='Experimental data')
-        plt.title('Experimental conditions : ' + exp_cond_param_annot + ' = ' + str(exp_cond_param_values))
+        plt.title('Experimental conditions : ' + exp_cond_param_annot + ' = '
+                  + str(exp_cond_param_values))
         plt.xlabel('Time')
         plt.ylabel(data_exp.fitting_instructions.sim_output_param_annot)
         plt.legend()
