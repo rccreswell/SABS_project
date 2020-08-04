3f5b9c6fbf95a9b3838c327b04390a2db382e6e7
barraly <yann-stanislas.barral@wolfson.ox.ac.uk>
2020-08-04 18:51:31 +0200

    Example loading CellML

--- Examples/Example loading CellML model/example.py
+++ Examples/Example loading CellML model/example.py
@@ -9,12 +9,18 @@ mmt = 'Examples/Example loading CellML model/ten_tusscher_model_2006_IK1Ko_epi_u
 # Load the CellML file and save the converted MMT model
 simulation = sabs_pkpd.load_model.load_model_from_cellml(cellml, mmt)
 
+""""
+MAKE SURE THAT THE STIMULUS IS SET CORRECTLY BEFORE PROCEEDING TO THE FOLLOWING PART
+""""
+# Reload the model in case stimulation protocol changes were needed
+simulation = sabs_pkpd.load_model.load_simulation_from_mmt(mmt)
+
 # Run directly using Myokit and plot the output
 result = simulation.run(1000, log_interval = 1)
-plt.plot(result['membrane_potential.V_m'])
+plt.plot(result['membrane.V'])
 
 # Run using the package
-result = sabs_pkpd.run_model.quick_simulate(simulation, 1000, 'membrane_potential.V_m',
+result = sabs_pkpd.run_model.quick_simulate(simulation, 1000, 'membrane.V',
                                             time_samples = np.linspace(0, 1000, 1001))
 plt.plot(result[0])
 
